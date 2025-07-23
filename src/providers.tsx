213623f0096f9cbd57da4b2f0d3ff602c8bdd6c7
sinzek <chase.p.brock@gmail.com>
2025-07-23 13:11:23 -0500

    default dark mode

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
 import { ReactQueryProvider } from "./context/react-query";
 import { ProjectProvider } from "./context/project-context";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-			<ReactQueryProvider>
-				<ProjectProvider>{children}</ProjectProvider>
-			</ReactQueryProvider>
-		</ThemeProvider>
+		<ReactQueryProvider>
+			<ProjectProvider>{children}</ProjectProvider>
+		</ReactQueryProvider>
 	);
 }
